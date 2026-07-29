f31fb600b3d2aadd289a63fa84fabac13b410c7d
Kamonphan Mongkonsin <Kamonphan.m@ku.th>
2026-07-29 18:05:28 +0700

    jun

--- server.js
+++ server.js
@@ -39,6 +39,30 @@ app.get('/api/products', (req, res) => {
       stock: 15,
       category: 'Projector',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmaN2pdHvbH1EvXrMiRyOEG4KQsx1v0i1PBmBgl9ulxR2RonnlcZWGRXK&s=10'
+    },
+    {
+      id: 4,
+      name: 'ACER ACER Projector x 1328wi',
+      price: 17390,
+      stock: 15,
+      category: 'Projector',
+      image: 'https://img.advice.co.th/cdn-cgi/image/format=auto,width=700,quality=82,fit=contain/images_nas/pic_product4/A0145144/A0145144OK_BIG_1.jpg'
+    },
+    {
+      id: 5,
+      name: 'Epson EPSON Projector / EB-E24',
+      price: 17790,
+      stock: 25,
+      category: 'Projector',
+      image: 'https://www.smartmediaprojector.com/uploads/6426/shop/202604/202604-27-142331_Wz-0.png'
+    },
+    {
+      id: 5,
+      name: 'Xiaomi Mi Smart Projector 2 Pro',
+      price: 23999,
+      stock: 10,
+      category: 'Projector',
+      image: 'https://media-cdn.bnn.in.th/151919/Xiaomi-Mi-Smart-Projector-2-White-2.jpg'
     }
   ]);
 });
